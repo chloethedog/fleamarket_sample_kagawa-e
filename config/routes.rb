ff01683cb8146db9edb6833dfecca4f8073d5339
Tatuno10 <zell8130@outlook.jp>
2021-01-20 14:58:17 +0900

    add-routing-item#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resources :item, only: :show
+  # あとで消す
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
