1a8a079db62aaf6500d0e3ef9b39969fe388a51a
takaokaSEA <uiuiui0112@icloud.com>
2021-01-23 20:33:55 +0900

    devise-viewsで新規登録・ログイン以外削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_112759) do
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
