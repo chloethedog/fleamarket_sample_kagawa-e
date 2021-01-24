178f525b9fd89497eb4938048cc35916b97faaf0
umesawah101023 <ume.h.ume101023@gmail.com>
2021-01-24 21:58:17 +0900

    [作成]purchases_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resources :items do
+    resources :purchases, only:[:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
