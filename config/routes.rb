9730e30dfb1aa48a6faf49af9bc78b68bf8f3feb
Tatuno10 <zell8130@outlook.jp>
2021-01-27 16:28:38 +0900

    fix-delete-useless-commentout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  # あとで消す
   resources :items
-  # あとで消す
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
