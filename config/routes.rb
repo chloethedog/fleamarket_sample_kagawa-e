80d2dc44efa5348db884796adb94e1fd6ddb40a9
yanotyan <m.parfume.y.ha7@gmail.com>
2021-01-28 19:57:07 +0900

    修正
    ハッシュロケットをシンボル型に
    Pathを”＃”に
    ルーティングをオンリー仕様に

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :users
+  resources :users, only:[:show]
   resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
