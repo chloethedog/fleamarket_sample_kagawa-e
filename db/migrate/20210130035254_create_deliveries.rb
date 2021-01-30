f503f1a7bfdad4a543d2ac2605f272513be7ffba
Tatuno10 <zell8130@outlook.jp>
2021-01-30 13:03:21 +0900

    Add-columns-and-constraints-to-the-delivery_table(deliveryテーブルにカラムと制約を追加した)

--- db/migrate/20210130035254_create_deliveries.rb
+++ db/migrate/20210130035254_create_deliveries.rb
@@ -1,7 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
-
+      t.string :postal_code, null: false
+      t.integer :prefectures_id, null: false
+      t.string :municipality, null: false
+      t.string :address, null: false
+      t.string :address_detail, null: false
+      t.string :phone_number
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
