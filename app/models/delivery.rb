8f25afbc536d737731bab0f5587f3d0428ecb1d6
Tatuno10 <zell8130@outlook.jp>
2021-01-30 14:44:09 +0900

    Add-validation-of-user_table(userテーブルのバリデーションを追加した)

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,6 @@
 class Delivery < ApplicationRecord
   belongs_to_active_hash :prefectures
   belongs_to :user, optional: true
-  
+
   validates :postal_code, :prefectures_id, :municipality, :address ,presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :last_name, :first_name, :last_name_ruby, :first_name_ruby, :birthday ,presence: true
+  
+  has_one :delivery
+  has_one :card 
+  has_many :items
+
+  validates :nickname, :birthday ,presence: true
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/, message: 'は＠とドメインを含む必要があります'}
+                    
+  validates :encrypted_password, confirmation: true
+  validates :encrypted_password, :encrypted_password_confirmation ,presence: true ,length: { minimum: 7 }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください'} do
+    validates :last_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ぁ-ん--]+\z/, message: 'に全角ひらがなを使用してください'} do
+    validates :last_name_ruby
+    validates :first_name_ruby
+  end
+
 end
