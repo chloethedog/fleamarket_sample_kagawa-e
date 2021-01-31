5ad575e5001fcfa8e73390670e0857adadb6e166
Tatuno10 <zell8130@outlook.jp>
2021-01-31 16:50:48 +0900

    Add-test-in-delivery_table(deliveryのテストを追加)

--- spec/models/delivery_spec.rb
+++ spec/models/delivery_spec.rb
@@ -5,5 +5,40 @@ describe "delivery" do
       delivery = build(:delivery)
       expect(delivery).to be_valid
     end
+
+    it "is invalid without postal_code" do
+      delivery = build(:delivery, postal_code: "")
+      delivery.valid?
+      expect(delivery.errors[:postal_code]).to include("can't be blank")
+    end
+
+    it "is invalid without prefecture_id" do
+      delivery = build(:delivery, prefectures_id: "")
+      delivery.valid?
+      expect(delivery.errors[:prefectures_id]).to include("can't be blank")
+    end
+
+    it "is invalid without municipality" do
+      delivery = build(:delivery, municipality: "")
+      delivery.valid?
+      expect(delivery.errors[:municipality]).to include("can't be blank")
+    end
+
+    it "is invalid without address" do
+      delivery = build(:delivery, address: "")
+      delivery.valid?
+      expect(delivery.errors[:address]).to include("can't be blank")
+    end
+
+    it "valid without address_detail" do
+      delivery = build(:delivery, address_detail: "")
+      expect(delivery).to be_valid
+    end
+
+    it "valid without phone number" do
+      delivery = build(:delivery, phone_number: "")
+      expect(delivery).to be_valid
+    end
+
   end
 end
