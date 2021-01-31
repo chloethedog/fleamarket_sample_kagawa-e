493b3e081f926db3c8bbb9347a504883e07365ff
Tatuno10 <zell8130@outlook.jp>
2021-01-31 15:59:37 +0900

    Add-test-for-email_address(メールアドレスについてのテストを追加)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,37 @@ describe "user" do
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
+
+    it "is invalid without a email address" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      user = build(:user)
+      user.valid?
+      expect(user.errors[:email]).to include("has already been taken")
+    end
+
+    it "is invalid with Email addresses without @ and domain" do
+      user = build(:user, email: "test")
+      user.valid?
+      expect(user.errors[:email]).to include("は＠とドメインを含む必要があります")
+    end
+
+    it "is invalid with Email addresses without @ " do
+      user = build(:user, email: "test.tet")
+      user.valid?
+      expect(user.errors[:email]).to include("は＠とドメインを含む必要があります")
+    end
+    
+    it "is invalid with Email addresses without domain " do
+      user = build(:user, email: "test@test")
+      user.valid?
+      expect(user.errors[:email]).to include("は＠とドメインを含む必要があります")
+    end
   end
 
 end
