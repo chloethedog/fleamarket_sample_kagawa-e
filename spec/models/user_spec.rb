2d34754ac5145275d93db26ade06458e313851c9
Tatuno10 <zell8130@outlook.jp>
2021-01-31 15:58:49 +0900

    Add-test-for-nickname(ニックネームについてのテストを追加)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,12 @@ describe "user" do
       user = build(:user)
       expect(user).to be_valid
     end
+
+    it "is invalid without a nickname" do
+      user = build(:user, nickname: "")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
   end
 
 end
