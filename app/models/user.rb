02501c47dffa7ec236f43e4f4af1f2b79fbf6f67
Tatuno10 <zell8130@outlook.jp>
2021-01-31 15:55:39 +0900

    Add-case_sensitive: false-to-password(passwordにcase_sensitive: falseを追加)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,17 @@ class User < ApplicationRecord
 
   validates :nickname, :birthday ,presence: true
 
-  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/, message: 'は＠とドメインを含む必要があります'}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A\S+@\S+\.\S+\z/, message: 'は＠とドメインを含む必要があります'}
                     
-  validates :encrypted_password, confirmation: true
-  validates :encrypted_password, :encrypted_password_confirmation ,presence: true ,length: { minimum: 7 }
+  validates :password, confirmation: true
+  validates :password, :password_confirmation ,presence: true ,length: { minimum: 7 }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください'} do
     validates :last_name
     validates :first_name
   end
   
-  with_options presence: true, format: { with: /\A[ぁ-ん--]+\z/, message: 'に全角ひらがなを使用してください'} do
+  with_options presence: true, format: { with: /\A[ぁ-ん-]+\z/, message: 'に全角ひらがなを使用してください'} do
     validates :last_name_ruby
     validates :first_name_ruby
   end
