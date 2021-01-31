22b4a880f1d35f8ac84b84aa5368a9a2e6494a25
Tatuno10 <zell8130@outlook.jp>
2021-01-31 16:31:00 +0900

    Add-prefecture-model-assciation(都道府県モデルへのアソシエーションを追加)

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,5 +1,6 @@
 class Delivery < ApplicationRecord
-  belongs_to_active_hash :prefectures
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
 
   validates :postal_code, :prefectures_id, :municipality, :address ,presence: true
