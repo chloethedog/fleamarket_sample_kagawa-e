b5b155ab1f7379941ab38a84b0e0db2438eed3e7
Tatuno10 <zell8130@outlook.jp>
2021-02-02 16:40:27 +0900

    Add-get:_card-to-users-routing(ユーザのルーティングにget: cardを追加した。)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :users, only:[:show]
+  resources :users, only:[:show] do
+    member do
+      get :card
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items, only: [:index, :show] do
     resources :purchases, only:[:index]
