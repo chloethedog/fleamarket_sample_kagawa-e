65b923906ff51773c958b01c8bdb9f1dcbbcd5f1
umesawah101023 <ume.h.ume101023@gmail.com>
2021-02-04 23:33:42 +0900

    pankuzu-list

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -8,16 +8,10 @@ crumb :user do
   parent :root
 end
 
-
-# crumb :purchases_index do |item|
-#   link "商品購入ページ",  item_purchases_path(item)
-#   parent :items,item
-# end
-
-# crumb :items_new do
-#   link "商品出品ページ", new_item_path
-#   parent :root
-# end
+crumb :item do
+  link "商品ページ", item_path
+  parent :root
+end
 
 #   link "Projects", projects_path
 # end
