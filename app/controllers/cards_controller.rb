e492e5362ab8d6eb0edc0ea9dbbf4fbcf8b1ac25
Tatuno10 <zell8130@outlook.jp>
2021-02-05 16:40:54 +0900

    Fix-create-year/month-variable-in-controller(コントローラに年月の変数を作った)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,17 @@
 class CardsController < ApplicationController
+  require "date"
+  def new
+    date = Date.today
+    year = date.year
+    after_year = year + 9
+    @years = []
+    year.upto(after_year) do |num|
+      @years << num
+    end
+    @month = []
+    11.times do |num|
+      num += 1
+      @month << num
+    end
+  end
 end
