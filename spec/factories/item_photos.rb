a8df0cb0e2b6cdd8a5443609cd65927a511124ab
yanotyan <m.parfume.y.ha7@gmail.com>
2021-02-10 20:25:40 +0900

    テストコード記述

--- spec/factories/item_photos.rb
+++ spec/factories/item_photos.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
 
   factory :item_photo do
-    thumbnail         {File.open("#{Rails.root}/public/images/test_image.jpg")}
-    item              {"1"}
-
+    thumbnail         { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec/fixtures/test_image.jpg"), 'image/jpg') }
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,9 +3,8 @@ FactoryBot.define do
   factory :item do
     name                   {"item"}
     price                  {1000}
-    purchase               {"1"}
+    purchase               {1}
     explanation            {"説明文"}
-    
     state_id               {1}
     brand                  {"クロユニ"}
     delivery_fee_id        {1}
@@ -17,5 +16,9 @@ FactoryBot.define do
     association :seller, factory: :user
     association :buyer, factory: :user
     association :category
+
+    after(:build) do |item|
+      item.item_photo = FactoryBot.build(:item_photo, item: item)
+    end
   end
 end
