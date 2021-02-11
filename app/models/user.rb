7d7b18126a3c07da25e114fd03dfd2a3f7903b52
Tatuno10 <zell8130@outlook.jp>
2021-02-11 15:18:10 +0900

    Fix-validation-to-devise(バリデーションをdeviseの物に修正)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :validatable
   
   before_save { email.downcase! }
 
   has_one :delivery
   has_one :card 
+
   has_many :items, class_name: 'Item', foreign_key: "seller_id"
   has_many :items, class_name: 'Item', foreign_key: "buyer_id"
-
-  validates :email, uniqueness: { case_sensitive: false, message: 'は既に登録されています。' }, format: { with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/, message: 'は＠とドメインを含む必要があります'}
-                    
-  validates :password, confirmation: { message: 'パスワードが間違っています。'  }
-  validates :password, length: { in: 7..126,message: 'は7文字以上126文字以下で入力してください'}
 
   with_options presence: {message: 'は空で入力しないでください。'} do
     validates :nickname
