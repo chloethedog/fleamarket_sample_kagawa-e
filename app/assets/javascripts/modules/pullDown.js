004c5c5aedd12076ebae2ff51e502ba678b297d3
yanotyan <m.parfume.y.ha7@gmail.com>
2021-02-11 11:41:56 +0900

    コードレビュー修正分

--- app/assets/javascripts/modules/pullDown.js
+++ app/assets/javascripts/modules/pullDown.js
@@ -3,12 +3,9 @@ $(function(){
   function buildForm(categories){
     let options='';
     categories.forEach(function (category) { 
-      options += `
-                  <option value="${category.id}">${category.name}</option>
-                 `;
+      options += ` <option value="${category.id}">${category.name}</option> `;
     });
 
-    
     const html=`
       <select class="form-pull-down select-category"  name="item[category_id]">
         <option value="">---</option>
@@ -17,15 +14,9 @@ $(function(){
     `
     return html
   }
-
-
-//changeイベント設定箇所
   $('.Categories').on('change','.form-pull-down',function(){
-    // alert('change!')
-    $(this).nextAll().remove()  //選択された要素より下のformを一度リセット
-    const category_id=$(this).val() //選択されたidを取得
-
-  //Ajaxのリクエストを記述
+    $(this).nextAll().remove()  
+    const category_id=$(this).val() 
     $.ajax({
       type: "GET",
       url: "/api/items/category",
@@ -33,16 +24,12 @@ $(function(){
       dataType: 'json'
     })
     .done(function(categories) {
-
-  
-      if(categories.length==0){ //レスポンスでカテゴリーがない場合は処理を行わない
+      if(categories.length==0){ 
         return false
       }
-      
-      const select_form=buildForm(categories) //挿入するフィームを作成
-      const target=$('.Categories') //append先である既存の要素を定義
+      const select_form=buildForm(categories) 
+      const target=$('.Categories') 
       target.append(select_form)
-    
     })
     .fail(function() {
       alert('失敗')
