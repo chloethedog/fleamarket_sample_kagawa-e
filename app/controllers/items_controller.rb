d41eb44f9bcfbb4d72a431df245684b98f24179c
Tatuno10 <zell8130@outlook.jp>
2021-02-14 12:37:23 +0900

    Add-edit,update-actions(edit,update アクションを追加)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,14 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
-    @category = Category.roots
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    
+    if @item.update(item_params)
+      redirect_to root_path, notice: '商品を編集しました'
+    else
+      flash.now[:alert] = @item.errors.full_messages
+      render :edit
+    end
   end
 
   def get_category
