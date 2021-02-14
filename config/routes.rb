44a5b26686f08b904dfa7fe04eed59356c3ad065
umesawah101023 <ume.h.ume101023@gmail.com>
2021-02-14 19:29:31 +0900

    ルーティングを整える

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   
   resources :items, only: [:index, :show, :new, :create, :edit, :update] do
     resources :purchases, only:[:index]
+    # get "items/new", to: ""
 
   end
 
