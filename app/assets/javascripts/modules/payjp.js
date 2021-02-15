ac199066f4d5c25df6d1df05351da220ca8b0fdf
yanotyan <m.parfume.y.ha7@gmail.com>
2021-02-15 23:08:16 +0900

    APIに値を渡すためのすり合わせ

--- app/assets/javascripts/modules/payjp.js
+++ app/assets/javascripts/modules/payjp.js
@@ -1,29 +1,31 @@
 $(function() {
   $('#card_form').on('submit', function(e) {
     e.preventDefault()
-    console.log(123)
-    Payjp.setPublicKey(['PAYJP_PUBLIC_KEY']);
+
+    Payjp.setPublicKey("pk_test_0653e560244cd04acd14b3ef");
     var card = {
-      number: document.getElementById("card-number").value,
-      exp_month: document.getElementById("credit_exp_month").value,
-      exp_year: document.getElementById("credit_exp_year").value,
-      cvc: document.getElementById("cvc").value
+      number: document.getElementById("card_number").value,
+      exp_month: document.getElementById("card[exp_month]").value,
+      exp_year: document.getElementById("card[exp_year]").value,
+      cvc: document.getElementById("card[security]").value
     };
+    console.log(card);
     if (card.number == "" || card.cvc == "") {
       alert("入力もれがあります");
     } else {
       Payjp.createToken(card, function(status, response) {
         if (status === 200 ) {
-          $("#card_number").removeAttr("name");
-          $("#cvc").removeAttr("name");
-          $("#exp_month").removeAttr("name");
-          $("#exp_year").removeAttr("name");
+          $("card_number").removeAttr("name");
+          $("card[security]").removeAttr("name");
+          $("card[exp_month]").removeAttr("name");
+          $("card[exp_year").removeAttr("name");
           $("#card_token").append(
             $('<input type="hidden" name="payjp-token">').val(response.id)
           );
-          $('#cardCreateForm').get(0).submit();
+          $('#card_form').get(0).submit();
           alert("登録に成功しました");
         } else {
+ 
           alert("カード情報が正しくありません");
         }
       });
