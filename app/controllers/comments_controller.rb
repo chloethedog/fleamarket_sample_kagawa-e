d2f6f0022443557f170b05d851717838b2581707
Tatuno10 <zell8130@outlook.jp>
2021-02-17 01:26:37 +0900

    Add-create-action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
-  def new
+  def index
   end
 
   def create
+    @comment = @item.comments.new(comment_params)
+    if @comment.save
+      redirect_to item_path(@item), notice: 'コメントが送信されました'
+    else
+      @comment =  @item.comments.includes(:item)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render item_path(@item)
+    end
   end
 end
