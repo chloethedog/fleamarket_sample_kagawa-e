07d9c11cc58444ce8905fe7e38cba19a3fdbfbd1
Tatuno10 <zell8130@outlook.jp>
2021-02-17 02:32:38 +0900

    Add-creat_action-and-return_method-to-comment-controller(コメントのコントローラーにcreateアクションとreturnメソッドを追加)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,29 @@
 class CommentsController < ApplicationController
-  def index
-  end
+  before_action :set_item
+  
 
   def create
     @comment = @item.comments.new(comment_params)
     if @comment.save
       redirect_to item_path(@item), notice: 'コメントが送信されました'
     else
-      @comment =  @item.comments.includes(:item)
+      @comments =  @item.comments.includes(:item)
       flash.now[:alert] = 'メッセージを入力してください。'
       render item_path(@item)
     end
+  end
+
+  def new
+    return @comment
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :item_id).merge(user_id: current_user.id)
+  end
+
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
