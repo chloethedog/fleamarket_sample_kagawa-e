b91f677f5d81c9207fb6e4dac8bed0146f699d2c
yanotyan <m.parfume.y.ha7@gmail.com>
2021-02-19 20:30:02 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,30 +11,47 @@ Rails.application.routes.draw do
   resources :users, only:[:show] do
     member do
       get :card
-      
     end
+    resources :cards, only:[:new, :create, :show, :destroy]
   end
-  
   resources :items do
-    resources :purchases, only:[:index,] do
-      collection do
-        get :pay
-      end
-    end
-  end
-
-  
-
-  resources :items do
-    resources :purchases, only:[:index]
     collection do
       get :search
-    end  
-    resources :comments, only: :create
-    namespace :api do
-      resources :comments, only: :index, default: {format: 'json' } 
     end
+    resources :purchases, only:[:index] do
+      collection do
+        get :pay
+      end 
+    end 
+      resources :comments, only: [:create] do
+      namespace :api do
+        resources :comments, only: :index, default: {format: 'json' } 
+      end
+end
+  #   resources :items do
+  #     resources :purchases, only: [:index] do
+  #       collection do
+  #         get :pay
+  #         get :search
+  #       end
+  #     end
+  #     resources :comments, only: [:create] do
+  #     namespace :api do
+  #       resources :comments, only: :index, default: {format: 'json' } 
+  #     end
+  #   end
+  # resources :items do
+  #   resources :purchases, only:[:index,] do
+  #     collection do
+  #       get :pay
+  #     end
+  #   end
+  #   resources :cards, only:[:new, :create, :show, :destroy]
+
   end
+
+  
+
 
   resources :cards, only:[:new, :create, :show, :destroy]
 end
