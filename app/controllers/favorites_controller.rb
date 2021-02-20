bb0d9acb0b8fc476bcfd688446a21abad9b6d9d6
Tatuno10 <zell8130@outlook.jp>
2021-02-20 10:36:43 +0900

    Add-controller-for-the-favorite-function(お気に入り機能のコントローラーを追加)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,25 @@
 class FavoritesController < ApplicationController
+  before_action :set_item
+
+  def create
+    @favorite = Favorite.find_by(item_id: @item.id, user_id: current_user.id)
+    if @favorite
+      redirect_to item_path(@item)
+    else
+      @favorites = @item.favorites.create(favorite_params)
+      redirect_to item_path(@item)
+    end
+  end
+
+
+  private
+
+  def favorite_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
